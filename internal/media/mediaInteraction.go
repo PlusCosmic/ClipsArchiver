fcebbb74e8a1bda0d220906c3de521302de576dc
Harry Leach <harry@pluscosmic.dev>
2024-03-24 21:39:57 +0000

    adding trim video to mediaInteraction.go

--- internal/media/mediaInteraction.go
+++ internal/media/mediaInteraction.go
@@ -18,8 +18,17 @@ func GenerateThumbnailFromVideo(input string, output string) error {
 	return err
 }
 
-func TrimVideoFile(input string, output string, startTime int, endTime int) {
+func TrimVideoFile(input string, output string, startTimeSeconds int, endTimeSeconds int) error {
+	stream := ffmpeg_go.Input(input, nil)
+	err := stream.Trim(ffmpeg_go.KwArgs{"start_frame": startTimeSeconds * 60, "end_frame": endTimeSeconds * 60}).Output(output).OverWriteOutput().Run()
+	return err
+}
 
+func CombineVideoFiles(input1 string, input2 string, output string) error {
+	stream1 := ffmpeg_go.Input(input1, nil)
+	stream2 := ffmpeg_go.Input(input2, nil)
+	err := ffmpeg_go.Concat([]*ffmpeg_go.Stream{stream1, stream2}).Output(output).OverWriteOutput().ErrorToStdOut().Run()
+	return err
 }
 
 func GetVideoProbeData(path string) (*ffprobe.ProbeData, error) {
